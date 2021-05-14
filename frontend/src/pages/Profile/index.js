bca69097d675393ee854f48be14c5ff8b6650367
Bruno Souza <bruno_augustosjc@hotmail.com>
2021-05-14 00:43:52 -0300

    update

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+import api from '../../services/api';
+
 import './style.css';
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
+  const ongName = localStorage.getItem('ongName');
+  const ongID = localStorage.getItem('ongId');
+
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: ongId,
+      }
+    }).then(response => {
+      setIncidents(response.data);
+
+    })
+  }, [ongId]);
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="be the hero" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
 
         <Link className="button " to="/incidents/new">Cadastrar novo Caso</Link>
         <button type="button" >
@@ -18,62 +36,21 @@ export default function Profile() {
       </header>
       <h1>Casos Cadastrados</h1>
       <ul>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
+        {incidents.map(incidents => (
+          <li key={incidents.id}>
+            <strong>CASO:</strong>
+            <p>{incidents.title}</p>
 
-          <strong>Descrição </strong>
-          <p>Descrição teste</p>
+            <strong>DESCRIÇÃO</strong>
+            <p>{incidents.description}</p>
 
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8c3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>Descrição </strong>
-          <p>Descrição teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8c3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>Descrição </strong>
-          <p>Descrição teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8c3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>Descrição </strong>
-          <p>Descrição teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8c3" />
-          </button>
-        </li>
+            <strong>VALOR</strong>
+            <p> {incidents.value} </p>
+            <button type="button">
+              <FiTrash2 size={20} color="#a8a8b3" />
+            </button>
+          </li>
+        ))}
       </ul>
 
     </div>
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import api from '../../services/api';
 import './style.css';
@@ -12,7 +12,7 @@ export default function Register() {
   const [city, setCity] = useState('');
   const [uf, setUf] = useState('');
 
-
+  const history = useHistory();
 
   async function handleRegister(e) {
     e.preventDefault();
@@ -26,8 +26,10 @@ export default function Register() {
     try {
       const response = await api.post('ongs', data);
       alert(`Seu ID de acesso: ${response.data.id}`);
+      history.push('/');
     } catch (err) {
       alert('Erro no cadastro, tente novamente');
+
     }
 
 
